10d327ab13de28df41b4e9b02ee260d2418983fb
Chris Waschenko <cwaschenk03@gmail.com>
2024-12-12 14:28:28 -0500

    added reformat and abstraction of PID output

--- include/Main/main.hxx
+++ include/Main/main.hxx
@@ -4,38 +4,65 @@
 #include <QtWidgets>
 #include <string.h>
 #include <QtCharts/QChartView>
-#include <QtCharts/QLineSeries>
+#include <QtCharts/QSplineSeries>
 #include <QtCharts/QDateTimeAxis>
 #include <QtCharts/QValueAxis>
 using namespace QtCharts;
+
+/** @brief Inputbox container */
+struct InputBox : public QGroupBox {
+  QVBoxLayout layout;
+  QSpinBox select;
+  InputBox(std::string title, QWidget* parent = nullptr) : QGroupBox(QString::fromStdString(title),parent), select(parent) {
+    layout.addWidget(&select);
+    this->setLayout(&layout);
+    this->setStyleSheet("QGroupBox {background: #242424;} QGroupBox:Title { color: white; }");
+  }
+
+};
+
+struct PIDOutput : public QGroupBox {
+  QGridLayout layout;
+  QChartView chart_view;
+  QChart chart;
+  QSplineSeries temp_over_time_series;
+
+  PIDOutput(std::string title, QWidget* parent = nullptr) : QGroupBox(QString::fromStdString(title),parent) {
+    layout.addWidget(&chart_view); 
+    this->setLayout(&layout);
+    this->setStyleSheet("QGroupBox {background: #242424;} QGroupBox:Title { color: white; }");
+  }
+
+};
+
 class Main : public QMainWindow
 {
   private:
   QWidget central_widget;
   QGridLayout layout;
-  QSpinBox ki_select;
-  QSpinBox kp_select;
-  QSpinBox kd_select;
-  QSpinBox temp_coefficient;
+  InputBox ki_select;
+  InputBox kp_select;
+  InputBox kd_select;
+  InputBox temp_coefficient;
   QPushButton run_sim_btn;
-  QChartView temp_v_time_chart;
-  QLineSeries temp_over_time_series;
+  PIDOutput pid_output;
   public:
     Main(QWidget* parent = nullptr) : 
       QMainWindow(parent),
-      ki_select(),
-      kp_select(),
-      kd_select(),
-      temp_coefficient(),
+      ki_select("KI",this),
+      kp_select("KP",this),
+      kd_select("KD",this),
+      temp_coefficient("Temp Slope",this),
       run_sim_btn("Run Simulation"),
-      temp_v_time_chart()
+      pid_output("PID Simulation",this)
     {
-      layout.addWidget(&temp_coefficient, 0,0);
-      layout.addWidget(&run_sim_btn,1,0);
-      layout.addWidget(&kp_select,0,1);
-      layout.addWidget(&ki_select,1,1);
-      layout.addWidget(&kd_select,2,1);
-      layout.addWidget(&temp_v_time_chart,3,0,1,-1);
+      this->resize(900,600);
+      layout.addWidget(&run_sim_btn,0,0,1,-1);
+      layout.addWidget(&temp_coefficient, 1,0,1,-1);
+      layout.addWidget(&kp_select,2,0);
+      layout.addWidget(&ki_select,2,1);
+      layout.addWidget(&kd_select,2,2);
+      layout.addWidget(&pid_output,4,0,1,-1);
       central_widget.setLayout(&layout);
       this->setCentralWidget(&central_widget);
     }
